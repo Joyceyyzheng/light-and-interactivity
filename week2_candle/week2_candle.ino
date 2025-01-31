9814cf8b6e38a209676f9dcd32d05e90f49a4b75
Joyce Zheng <yz4867@nyu.edu>
2025-01-31 16:22:59 -0500

    hue good, breathing tgt

--- week2_candle/week2_candle.ino
+++ week2_candle/week2_candle.ino
@@ -14,72 +14,80 @@ int change = 10;             // increment to change hue by
 // set up strip:
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixelCount, neoPixelPin, NEO_GRBW + NEO_KHZ800);
 
-int h = 2000;  // hue
-int s = 255;  // saturation
-int minB = 50;  // intensity - min
-int maxB = 220; // intensity - max
+int h = 3800;  // hue hue<2000, reder
+int s = 250;  // saturation
+int minB = 80;  // intensity - min
+int maxB = 180; // intensity - max
 int fadeStep = 1;
-int hRange = 2000;
+int hRange = 1800; //2000-5600
 
 //defs for looping each one
-unsigned long interval = 2000;  
-unsigned long previousMillis = 0;  
-int currentPixel = 0;  
+// unsigned long interval = 2000;  
+// unsigned long previousMillis = 0;  
+// int currentPixel = 0;  
 
 //blue fire
 int bluePixel = 3;
 int blueBrightness = 0;
 int blueChange = 1;
 
+//sine curve
+unsigned long interval = 50;  
+unsigned long previousMillis = 0; 
+float brightnessPhase = 0;  
+
 void setup() {
   strip.begin();  // initialize pixel strip
   strip.clear();  // turn all LEDs off
   strip.show();   // update strip
 
-   randomSeed(analogRead(0));  //learned this from chat,randomize initial number
+  randomSeed(analogRead(0));  //learned this from chat,randomize initial number
+
+  
 }
 
 void loop() {
 
-   for (int pixel = 0; pixel < pixelCount; pixel++) {
-   
-    int hue = h + random(-hRange / 2, hRange / 2);  // randomize
-    int brightness = random(minB, maxB);    //randomize
+  //brightness
+   unsigned long currentMillis = millis();
 
-    
-    if (pixel >= 5) {  
-      brightness = random(minB + 80, maxB);
+  
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+  
+    brightnessPhase += 0.5;  // brightness changing speed ⚠️
+    if (brightnessPhase >= 2 * PI) {
+      brightnessPhase -= 2 * PI; 
     }
 
+    float brightnessSin = sin(brightnessPhase);  
+    Serial.print(brightnessSin);
+    Serial.println();
+    int brightness = map(brightnessSin * 1000, -1000, 1000, minB, maxB); 
+
+
+
+  //color
+
+   for (int pixel = 0; pixel < pixelCount; pixel++) {
    
-    long color = strip.ColorHSV(hue, 255, brightness);
+    int hue = h + random(-hRange / 2, hRange / 4);  // randomize, 2000-4250
+   // int brightness = random(minB, maxB);    //randomize
+    
+    // if (pixel >= 7) {  //control each light 
+    //   brightness = random(minB + 80, maxB); //cannot just random ig
+    // }
+
+    long color = strip.ColorHSV(2200, s, brightness);
     strip.setPixelColor(pixel, color);
   }
 
-  // //main part - orange, red
-  // for (int pixel = 0; pixel < pixelCount; pixel++) {
-  //   if (pixel != bluePixel) {  
-  //     long color = strip.ColorHSV(h, s, i);
-  //     strip.setPixelColor(pixel, color);
-  //   }
-  // }
-
-  // //blue part 
-  // blueBrightness += blueChange; 
-  // if (blueBrightness > 200 || blueBrightness < 50) {
-  //   blueChange = -blueChange;  
-  // }
-  // strip.setPixelColor(bluePixel, h, 0, 100); 
-
+  }
+ 
   strip.show();
-
-  //  i -= fadeStep;
-  // if (i <= 100 || i >= 180) {
-  //   fadeStep = -fadeStep;  
-  // }
-
   
-  delay(80); 
+  delay(100); 
 
 
 }
